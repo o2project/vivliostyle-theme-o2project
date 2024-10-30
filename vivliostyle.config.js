6a2bfe7b982fbbf085bc20a947b97ff6a9e8cfee
Shota Kubota <ta2@o2p.jp>
2024-10-31 01:13:52 +0900

    chore(vivliostyle): change entry order

--- vivliostyle.config.js
+++ vivliostyle.config.js
@@ -5,12 +5,16 @@ module.exports = {
   toc: true,
   tocTitle: '目次',
   entry: [
-    { rel: 'contents', theme: 'lib/toc.css' },
-    'example/default.md',
+    {
+      path: './example/book_title.md',
+      theme: './lib/book_title.css',
+    },
+    { rel: 'contents', theme: './lib/toc.css' },
+    './example/default.md',
     {
       title: 'おわりに',
-      path: 'example/book_information.md',
-      theme: 'lib/book_information.css',
+      path: './example/book_information.md',
+      theme: './lib/book_information.css',
     },
   ],
   output: [
